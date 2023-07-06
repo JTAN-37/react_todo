c819925c5fd33b8670cd4c269adbbbce9c999f82
Jeremy Tan <jeremytan@Jeremys-MacBook-Pro.local>
2023-07-06 15:50:54 +0800

    finish todo list functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,60 @@ import { useState } from "react"
 
 export default function App() {
   const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+  //Return value of setTodos is the new state
+
+  function handleSubmit(e) {
+    e.preventDefault() //Prevents a refresh
+
+    //To modify state, must pass function.
+    //Function returns new state
+    //Single argument is current state
+
+    //... is spread operater in JS, it takes out array's elements and puts them
+    //A UUID is a unique ID number. That's it.
+    setTodos( (currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ]
+    })
+
+    //Clears the search bar
+    setNewItem("")
+  }
+
+  //Change the todo with the id based on completed flag
+  //ANY TIME state is changed, must create new state object
+  function toggleTodo(id, completed) {
+
+    //we pass a function to setTodos (=> denotes a function)
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return {...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      //Returns an array without the todo's id that equals the passed id
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
   return (
   <>
-    <form className="new-item-form">
+    <form onSubmit={handleSubmit} className="new-item-form">
       <div className="form-row">
         <label htmlFor="item">New Item</label>
         <input 
           value={newItem}
-          //when there is a change, calll function to change value
+          //when there is a change, call function to change value
           //and rerun component
           onChange={e => setNewItem(e.target.value)}
           type="text"
@@ -21,13 +67,32 @@ export default function App() {
     </form>
     <h1 className="header">Todo List</h1>
     <ul className="list">
-      <li>
+      {//Short circuit logic to show "No Todos when no todo items
+      }
+      {todos.length === 0 && "No Todos"}
+      {//Curly brackets is JavaScript code
+      //Arrays in React need a key property. Why?
+      //Alerts React if any changes to element in array (optimize performance)
+      }
+      {todos.map(todo => {
+        return (
+        <li key={todo.id}>
         <label>
-          <input type="checkbox" />
-          Item 1
+          <input type="checkbox" checked={todo.completed}
+          //onChange listens for a click event, passes todo's id and checkbox state
+          onChange={e => toggleTodo(todo.id, e.target.checked)}/>
+
+          {todo.title}
         </label>
-        <button className="btn btn-danger">Delete</button>
+        <button 
+          //onClick calls a function, which calls deleteTodo
+          onClick={() => deleteTodo(todo.id)}
+          className="btn btn-danger">
+            Delete
+        </button>
       </li>
+        )
+      })}
     </ul>
   </>
   )
